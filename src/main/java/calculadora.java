8e74e6f0cb6aabf4df33c68d86b1c5bdc5b66383
joseluis87 <jlaa1@hotmail.com>
2022-02-23 07:29:14 +0000

    subiendo las operaciones multiplicacion y division a la nueva rama

--- src/main/java/calculadora.java
+++ src/main/java/calculadora.java
@@ -9,4 +9,25 @@
  */
 public class calculadora {
     
+    
+        public static int Multiplicacion(int a, int b) {
+        return a*b;
+    }
+        public static int Division(int a, int b) {
+        return a/b;
+    }
+        
+            public static void main(String args[])
+    {
+
+        
+        System.out.println("Multiplicación de 3 * 3 = "+ Multiplicacion(3,3));
+        System.out.println("Division de 5 / 5 = "+ Division(5,5));
+
+   
+    }
+        
+        
+        
+    
 }
