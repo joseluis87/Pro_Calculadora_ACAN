8b17811cde8539d55daa653dfb5071edd5690b25
joseluis87 <jlaa1@hotmail.com>
2022-02-23 08:23:32 +0000

    subiendo las operaciones sumas y restas a la nueva rama

--- src/main/java/calculadora.java
+++ src/main/java/calculadora.java
@@ -7,6 +7,27 @@
  *
  * @author ceti
  */
-public class calculadora {
+public class calculadora { 
+    
+    
+    public static int Suma(int a, int b) {
+        return a+b;
+    }
+    public static int Resta(int a, int b) {
+        return a-b;
+    }
+    
+    
+        public static void main(String args[])
+    {
+        System.out.println("Suma de 1 + 1 = "+ Suma(1,1));
+        System.out.println("Resta de 2 - 1 = "+ Resta(2,1));
+
+    }
+    
+    
+    
+    
+    
     
 }
